ab9869f883de88d261afb79b63bbf061b3e10f97
Bruno Testa <brunotesta1912@gmail.com>
2022-05-13 22:35:25 -0400

    Addition of phone number JavaScript validation

--- java.js
+++ java.js
@@ -21,3 +21,26 @@ function validateName() {
     return true;
 
 }
+let check = document.querySelector(".check");
+let number = document.querySelector(".number");
+let text = document.querySelector(".text");
+
+
+let regex = /^[\d,\s,\+,\-]{5,20}/;
+
+check.addEventListener("click",()=>{
+    if(number.value =="") {
+        Text.innerText = "Please Enter Your Phone Number";
+        Text.style.color = "#fff";
+    }
+    else if(number.value.match(regex)) {
+        Text.innerText = "Phone Number Validated";
+        Text.style.color = "rgba(205, 209, 228);, 1";
+    }
+    else 
+    {
+        Text.innertext = "Oops! Your Phone Number Is Invalid";
+        Text.style.color = "";
+    }
+
+})
